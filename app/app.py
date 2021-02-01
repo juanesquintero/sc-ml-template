5e33a4f014f87a3e37a2d368fb24d18e8a2b6bba
Juanes Quintero <juaneschrome@gmail.com>
2021-02-01 11:28:02 -0500

    folder structure definition

---
+++ app/app.py
@@ -0,0 +1,20 @@
+# Import libraries
+from flask import Flask
+from flask_restful import Api, Resource
+
+
+app = Flask(__name__)
+api = Api(app=app, prefix='/api')
+
+
+app.config['JSON_SORT_KEYS'] = False
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/', '')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
